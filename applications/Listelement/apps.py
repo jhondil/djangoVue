db80c336d7f1d62d6fc537c2854826c092833d36
Jhonatan Ibarra (backend) <ibarra.jhonatann@gmail.com>
2021-10-04 09:57:46 -0500

    configuration of models and part admin listElement

--- applications/Listelement/apps.py
+++ applications/Listelement/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ListelementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Listelement'
+    name = 'applications.Listelement'
--- djangoVue/settings.py
+++ djangoVue/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'applications.listelement',
+    'applications.Listelement',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'djangoVue.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'UdemyTiendaOnline',
+        'USER': 'postgres',
+        'PASSWORD': 'devincap@3*',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
